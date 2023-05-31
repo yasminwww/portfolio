06c8ab7852ca858d5f108f952a827d4bdfba5b6f
yasminski <yasmin@homeq.se>
2023-05-31 15:50:56 +0200

    Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Yasmin's Portfolio</title>
-        <meta name="description" content="Joel's Portfolio" />
+        <title>Yasmins Portfolio</title>
+        <meta name="description" content="Yasmins Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
