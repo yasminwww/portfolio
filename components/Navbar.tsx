eb7aa345275c5bd0f88cd495a94d52481fca45a7
ym222bs <yasmin.mu13@gmail.com>
2023-10-27 12:49:09 +0200

    Fix background animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,10 +28,8 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    // Add a scroll event listener when the component mounts
     window.addEventListener("scroll", handleScroll);
 
-    // Clean up the event listener when the component unmounts
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -39,37 +37,37 @@ const Navbar = () => {
 
   return (
     <nav className="nav-container">
-      <div className="logo">
-        <Link href="/">Yasmins Portfolio</Link>
-      </div>
+      <div className="logo"></div>
+      <Link href="/">Yasmins Portfolio</Link>
       {/* <a
         href="https://docs.google.com/document/d/10ZosQ38Z3804KYPcb_aZp9bceoXK-q3GrkHjYshqIRE/edit?usp=sharing"
         className="cta-btn"
       >
         Resume
       </a> */}
-
-      <Link href="/" onClick={goToTop}>
-        Home
-      </Link>
-      <Link
-        href="#about"
-        onClick={() => scrollToSection("about-container", 250)}
-      >
-        About
-      </Link>
-      <Link
-        href="#projects"
-        onClick={() => scrollToSection("projects-container", 250)}
-      >
-        Projects
-      </Link>
-      <Link
-        href="#contact"
-        onClick={() => scrollToSection("contact-container", 0)}
-      >
-        Contact
-      </Link>
+      <div className="nav-links">
+        <Link href="/" onClick={goToTop}>
+          Home
+        </Link>
+        <Link
+          href="#about"
+          onClick={() => scrollToSection("about-container", 250)}
+        >
+          About
+        </Link>
+        <Link
+          href="#projects"
+          onClick={() => scrollToSection("projects-container", 250)}
+        >
+          Projects
+        </Link>
+        <Link
+          href="#contact"
+          onClick={() => scrollToSection("contact-container", 0)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,32 @@ export default function Home() {
         <link rel="icon" href="/y.png" />
       </Head>
       <div>
+        <section className="gradient-bg">
+          <div
+            data-w-id="4cd1b5f2-9d79-0946-28b2-89b46f675bde"
+            className="bg-circle _1"
+          ></div>
+          <div
+            data-w-id="4cd1b5f2-9d79-0946-28b2-89b46f675bdf"
+            className="bg-circle _2"
+          ></div>
+          <div
+            data-w-id="4cd1b5f2-9d79-0946-28b2-89b46f675be0"
+            className="bg-circle _3"
+          ></div>
+          <div
+            data-w-id="4cd1b5f2-9d79-0946-28b2-89b46f675be1"
+            className="bg-circle _4"
+          ></div>
+          <div
+            data-w-id="4cd1b5f2-9d79-0946-28b2-89b46f675be2"
+            className="bg-circle _5"
+          ></div>
+          <div
+            data-w-id="4cd1b5f2-9d79-0946-28b2-89b46f675be3"
+            className="bg-circle _6"
+          ></div>
+        </section>
         <Hero />
         <div
           style={{
