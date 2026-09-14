f44a8c6a803ea7d72873ee4424dce151ae2c6089
yasminwww <yasmu@eg.se>
2026-09-14 09:25:37 +0200

    update copy

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,9 +3,8 @@ const Contact = () => {
     <div className="contact-container" id="contact-container">
       <h2>Get In Touch</h2>
       <p>
-        If you want us to work together, have any questions or want me to speak
-        at your event, my inbox is always open. Whether I just want to say hi,
-        Ill try my best to get back to you! Cheers!
+        If you want us to work together or have any questions, my inbox is always open. Whether I just want to say hi,
+        I'll try my best to get back to you!
       </p>
       <a href="mailto:yasmin.k.mushahdi@gmail.com" className="cta-btn">
         Say Hello
